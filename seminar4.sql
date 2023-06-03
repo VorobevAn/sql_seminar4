a1ef42f515c8f20e1bb4dd18224d9d8a62392607
VorobevAn <114291056+VorobevAn@users.noreply.github.com>
2023-06-03 13:22:55 +0500

    Add files via upload

---
+++ seminar4.sql
@@ -0,0 +1,67 @@
+use seminar4;
+
+CREATE TABLE if not exists AUTO 
+(       
+	REGNUM VARCHAR(10) PRIMARY KEY, 
+	MARK VARCHAR(10), 
+	COLOR VARCHAR(15),
+	RELEASEDT DATE, 
+	PHONENUM VARCHAR(15)
+);
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM ) VALUES
+(111114,'LADA', 'КРАСНЫЙ', date'2008-01-01', '9152222221'),
+(111115,'VOLVO', 'КРАСНЫЙ', date'2013-01-01', '9173333334'),
+(111116,'BMW', 'СИНИЙ', date'2015-01-01', '9173333334'),
+(111121,'AUDI', 'СИНИЙ', date'2009-01-01', '9173333332'),
+(111122,'AUDI', 'СИНИЙ', date'2011-01-01', '9213333336'),
+(111113,'BMW', 'ЗЕЛЕНЫЙ', date'2007-01-01', '9214444444'),
+(111126,'LADA', 'ЗЕЛЕНЫЙ', date'2005-01-01', null),
+(111117,'BMW', 'СИНИЙ', date'2005-01-01', null),
+(111119,'LADA', 'СИНИЙ', date'2017-01-01', 9213333331);
+
+select * from AUTO;
+
+-- 1. Вывести на экран сколько машин каждого цвета для машин марок BMW и LADA
+select color 'Цвет', mark 'марка', count(*) 'колличество' 
+from auto 
+where mark in ('BMW','LADA')
+group by mark, color;
+
+
+-- 2. Вывести на экран марку авто и количество AUTO не этой марки
+select  mark 'марка', 
+	count(*) 'Количество машин произодителя', 
+    (select sum(auto.total) 
+	from (select mark, count(*) total 
+		 from auto 
+		 group by mark
+         HAVING mark != a.mark) auto) 'машины других марок'
+from auto a
+group by mark;
+
+-- Даны 2 таблицы, созданные следующим образом:
+drop table test_a;
+drop table test_b;
+
+create table test_a (id int, data varchar(1));
+create table test_b (id int);
+insert into test_a(id, data) values
+(10, 'A'),
+(20, 'A'),
+(30, 'F'),
+(40, 'D'),
+(50, 'C');
+insert into test_b(id) values
+(10),
+(30),
+(50);
+/*
+Напишите запрос, который вернет строки из таблицы test_a, id которых нет в таблице test_b, 
+НЕ используя ключевого слова NOT.
+*/
+select a.*
+from test_a a 
+left join test_b b
+on a.id = b.id 
+where b.id is null;
